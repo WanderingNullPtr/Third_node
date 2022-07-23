259095abb1249f5dc8db39c72a0309807fb0c2c7
WanderingNullPtr <nb.mithai@gmail.com>
2022-07-23 20:05:09 +0530

    Build emake.1

--- screenshot.py
+++
@@ -1,6 +0,0 @@
-from PIL import ImageGrab
-import sys
-
-GameWindow = ImageGrab.grab( bbox = (int(sys.argv[2]),int(sys.argv[3]),
-                            int(sys.argv[4]),int(sys.argv[5])) )
-GameWindow.save(sys.argv[1])
